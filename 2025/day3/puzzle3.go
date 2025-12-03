b0aa5ef48e922a7875e7c106d02fa8d0e14ea3de
Daniel Shanker <daniel.shanker@indexexchange.com>
2025-12-03 13:09:22 -0500

    day3 goroutines

--- 2025/day3/puzzle3.go
+++ 2025/day3/puzzle3.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"math"
+	"sync"
+	"sync/atomic"
 	. "utils"
 )
 
@@ -19,51 +21,67 @@ func main() {
 func part1(lines []string) int {
 	answer := 0
 
+	var atomicAnswer atomic.Int64
+	wg := sync.WaitGroup{}
 	for _, line := range lines {
 		left := 0
 		right := 0
-		for i, r := range line {
-			cur := S2i(string(r))
-			if i < len(line)-1 {
-				if cur > left {
-					left = cur
-					right = 0
-					continue
+		wg.Add(1)
+		go func(s string) {
+			defer wg.Done()
+			for i, r := range s {
+				cur := S2i(string(r))
+				if i < len(s)-1 {
+					if cur > left {
+						left = cur
+						right = 0
+						continue
+					}
+				}
+				if cur > right {
+					right = cur
 				}
 			}
-			if cur > right {
-				right = cur
-			}
-		}
-		answer += left*10 + right
+			atomicAnswer.Add(int64(left*10 + right))
+		}(line)
 	}
 
+	wg.Wait()
+	answer = int(atomicAnswer.Load())
 	return answer
 }
 
 func part2(lines []string) int {
 	answer := 0
+	var atomicAnswer atomic.Int64
 
+	wg := sync.WaitGroup{}
 	for _, line := range lines {
-		vals := make([]int, 12)
-		for i, r := range line {
-			cur := S2i(string(r))
-			for j := 0; j < 12; j++ {
-				if checkVal(i, len(line), 11-j, cur, vals[j]) {
-					vals[j] = cur
-					for k := j + 1; k < 12; k++ {
-						vals[k] = 0
+		wg.Add(1)
+		go func(s string) {
+			defer wg.Done()
+			vals := make([]int, 12)
+			for i, r := range s {
+				cur := S2i(string(r))
+				for j := 0; j < 12; j++ {
+					if checkVal(i, len(s), 11-j, cur, vals[j]) {
+						vals[j] = cur
+						for k := j + 1; k < 12; k++ {
+							vals[k] = 0
+						}
+						break
 					}
-					break
 				}
 			}
-		}
-		add := 0
-		for k := 0; k < 12; k++ {
-			add += vals[k] * int(math.Pow10(11-k))
-		}
-		answer += add
+			add := 0
+			for k := 0; k < 12; k++ {
+				add += vals[k] * int(math.Pow10(11-k))
+			}
+			atomicAnswer.Add(int64(add))
+		}(line)
 	}
+	wg.Wait()
+	answer = int(atomicAnswer.Load())
 
 	return answer
 }
